544610de3f31229a194279009bb8a7110e22dab4
Yua Shizuki <keshav@Keshavs-MacBook-Pro.local>
2016-01-15 14:26:17 +0530

    color

--- order_center/config/desktop.py
+++ order_center/config/desktop.py
@@ -5,7 +5,7 @@ from frappe import _
 def get_data():
 	return {
 		"order_center": {
-			"color": "Khaki",
+			"color": "MediumPurple",
 			"icon": "icon-suitcase",
 			"type": "module",
 			"label": _("order_center"),
